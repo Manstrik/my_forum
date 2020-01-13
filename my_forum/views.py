2466847ee3e5beeeeb5a8a63b271daaa3f60a6da
nikolay <zhigalin@fas.gov.ru>
2020-01-13 11:17:58 +0300

    Правка по flake8

--- my_forum/views.py
+++ my_forum/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 
 
 def handler404(request, exception):
+    """
+    Кастомная страница обработки ошибки 404.
+
+    :param request: объект запроса
+    :param exception: объект ошибки
+    :return: отрендеренный шаблон
+    """
     return render(request, 'error_pages/404.html', context={'exception': exception})
