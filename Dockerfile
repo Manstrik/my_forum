a0d69c0d5d614eb757c341475b592e170c6aa84c
nikolay <zhigalin@fas.gov.ru>
2020-01-14 17:57:41 +0300

    Возврат к чистому css

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,5 @@ RUN pip install -r requirements.txt \
 COPY . /app
 
 ENTRYPOINT python manage.py migrate \
-        && python manage.py compilescss \
-        && python manage.py collectstatic --noinput --ignore=*.scss \
+        && python manage.py collectstatic --noinput \
         && gunicorn my_forum.wsgi -b 0.0.0.0 --access-logfile - --log-file -
--- my_forum/settings.py
+++ my_forum/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sass_processor',
     'title_page',
 ]
 
@@ -120,11 +119,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
-]
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
 ]
 
 MEDIA_URL = '/media/'
