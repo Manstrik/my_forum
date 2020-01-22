dde9aaeb7e2d0ade198b495dd4dad7c10ac5c62f
nikolay <zhigalin@fas.gov.ru>
2020-01-22 17:12:16 +0300

    Фикс страницы 404

--- my_forum/views.py
+++ my_forum/views.py
@@ -3,12 +3,11 @@
 from django.shortcuts import render
 
 
-def handler404(request, exception):
+def handler404(request, *args, **kwargs):
     """
     Кастомная страница обработки ошибки 404.
 
     :param request: объект запроса
-    :param exception: объект ошибки
     :return: отрендеренный шаблон
     """
-    return render(request, 'error_pages/404.html', context={'exception': exception})
+    return render(request, 'error_pages/404.html')
