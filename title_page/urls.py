fb347c5d2433e9e4b6ae09c24fe044f81efc07d5
nikolay <zhigalin@fas.gov.ru>
2020-01-22 13:34:22 +0300

    Отдельный класс для детальной информации о посте + смена имен файлов

--- title_page/urls.py
+++ title_page/urls.py
@@ -2,12 +2,12 @@
 
 from django.urls import path
 
-from title_page.views import PostList
+from title_page.views import PostDetail, PostList
 
 app_name = 'title_page'
 
 urlpatterns = [
     # Главная страница приложения
     path('', PostList.as_view(), name='index'),
-    path(r'post/<int:pk>/', PostList.as_view(), name='post-detail')
+    path(r'post/<int:pk>/', PostDetail.as_view(), name='post-detail')
 ]
--- title_page/views.py
+++ title_page/views.py
@@ -1,13 +1,21 @@
 """Представления для приложения title_page."""
 
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from title_page.models import Post
 
 
 class PostList(ListView):
-    """Класс-представление основной страницы приложения title_page."""
+    """Класс-представление страницы с постами приложения title_page."""
 
-    template_name = 'title_page/posts.html'
+    template_name = 'title_page/post_list.html'
     model = Post
     context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    """Класс-представление страницы с детальной информацией о посте приложения title_page."""
+
+    template_name = 'title_page/post_list.html'
+    model = Post
+    context_object_name = 'post'
