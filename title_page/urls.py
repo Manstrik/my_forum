26be224205757172eaa0f33332ae537d70f0f238
nikolay <zhigalin@fas.gov.ru>
2020-01-22 14:16:07 +0300

    Сокращение урла к шаблону с детальной инфой о посте

--- title_page/urls.py
+++ title_page/urls.py
@@ -7,7 +7,8 @@ from title_page.views import PostDetail, PostList
 app_name = 'title_page'
 
 urlpatterns = [
-    # Главная страница приложения
-    path('', PostList.as_view(), name='index'),
-    path(r'post/<int:pk>/', PostDetail.as_view(), name='post-detail')
+    # Главная страница приложения со списком постов
+    path('', PostList.as_view(), name='post-list'),
+    # Страница с детальной информацией о посте
+    path(r'<int:pk>/', PostDetail.as_view(), name='post-detail')
 ]
