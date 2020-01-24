f36a216e5d98632c924cd01b38f8b71b1ef9e15b
Vladislav <vladeslaw@gmail.com>
2020-01-24 14:22:12 +0300

    Сделан запрос, в данный момент не работает

--- create_topic/views.py
+++ create_topic/views.py
@@ -22,12 +22,17 @@ def create_post(request):
     :param request: обхект запроса
     """
     text_field = None
+    text_post_name = None
     if request.method == 'POST':
         if 'text_field' in request.POST:
             text_field = request.POST['text_field']  # записываем значение в переменную без браузера
-    elif request.method == 'GET':  # присваиваем значение в строке браузера/переменной
+            text_post_name = request.POST['post_name']
+    if request.method == 'GET':  # присваиваем значение в строке браузера/переменной
         text_field = request.GET['text_field']
-    if text_field is not None:
-        create = CreatePost(post_text=str(text_field))
-        create.save()
+        text_post_name = request.GET['post_name']
+    if text_field and text_post_name is not None:
+        create_text = CreatePost(post_text=str(text_field))
+        create_post_name = CreatePost(post_name=str(text_post_name))
+        create_text.save()
+        create_post_name.save()
     return HttpResponse('Добавлено!')
