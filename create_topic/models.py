93392db17b62afda2269743f9f183988e67313ae
Vladislav <vladeslaw@gmail.com>
2020-01-28 17:38:48 +0300

    Добавил новый блок для отображения текущих постов.

--- create_topic/models.py
+++ create_topic/models.py
@@ -7,6 +7,10 @@ from django.db import models
 class CreatePost(models.Model):
     """Модель нового поста на форуме."""
     post_user_name = models.CharField(verbose_name='Имя пользователя', default='Не задано имя', max_length=128)
+    # нужно будет изменить, когда будет сделана регистрация
     post_name = models.CharField(verbose_name='Тема поста', default='Не задано имя поста', max_length=128)
+    # название поста
     post_text = models.TextField(verbose_name='Текст поста', default='Текст не введён')
+    # текст поста
     post_time = models.DateTimeField(verbose_name='Дата и время поста', auto_now_add=True)
+    # время создания поста
--- create_topic/views.py
+++ create_topic/views.py
@@ -18,22 +18,19 @@ class Index(ListView):
 def create_post(request):
     """
     Функция для создания поста.
-
     :param request: обхект запроса
     """
     text_field = None
     text_post_name = None
-    if request.method == 'POST':
+    if request.method == 'POST':  # если запрос пост, то
         if 'text_field' in request.POST:
-            text_field = request.POST['text_field']  # записываем значение в переменную без браузера
-            text_post_name = request.POST['post_name']
+            text_field = request.POST['text_field']  # текст поста
+            text_post_name = request.POST['post_name']  # название поста
     if request.method == 'GET':  # присваиваем значение в строке браузера/переменной
-        text_field = request.GET['text_field']
-        text_post_name = request.GET['post_name']
+        text_field = request.GET['text_field']  # текст поста
+        text_post_name = request.GET['post_name']  # название поста
     if text_field and text_post_name is not None:
-        create_text = CreatePost(post_text=str(text_field),
+        create_text = CreatePost(post_text=str(text_field),  # сохраняем в базу
                                  post_name=str(text_post_name))
         create_text.save()
-        # create_post_name = CreatePost(post_name=str(text_post_name))
-        # create_post_name.save()
     return HttpResponse('Добавлено!')
