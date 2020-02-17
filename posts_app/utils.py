18a5b8469c594c753b8d24f5e057531af24c00c8
Nikolay <nick.zhigalin@gmail.com>
2020-02-18 00:48:48 +0300

    Функция для рекурсивного удаления пустых папок

--- posts_app/utils.py
+++ posts_app/utils.py
@@ -1,5 +1,4 @@
 """Файл дополнительных утилит приложения posts_app."""
-
 import os
 
 TRASH_KEYS = ['csrfmiddlewaretoken']
@@ -26,3 +25,27 @@ def get_post_image_upload_path(instance, filename):
     :return: путь к файлу
     """
     return os.path.join('posts', instance.created_by.username, filename)
+
+
+def delete_empty_dirs(path: str):
+    """
+    Функция для рекурсивного удаления всех пустых папок.
+
+    :param path: абсолютный путь до папки, с которой начинать удаление
+    """
+    # Если переданное значение не является папкой, выходим из функции
+    if not os.path.isdir(path):
+        return
+
+    children_dirs = [
+        os.path.join(path, child_dir) for child_dir in os.listdir(path)
+        if os.path.isdir(os.path.join(path, child_dir))
+    ]
+
+    if len(children_dirs):
+        for child_dir in children_dirs:
+            delete_empty_dirs(child_dir)
+
+    # Саму папку media не удаляем
+    if not len(os.listdir(path)) and path[-5:] != 'media':
+        os.rmdir(path)
