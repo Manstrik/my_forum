811726c67ee853d1537d4b83360e38c80b4b072b
Nikolay <nick.zhigalin@gmail.com>
2020-02-21 20:50:01 +0300

    Добавлена страница входа

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,12 +1,19 @@
 """Файл маршрутов приложения users_app."""
 
 from django.urls import path
+from django.views.generic import TemplateView
 
-from users_app.views import logout
+from users_app.views import login, logout
 
 app_name = 'users_app'
 
 urlpatterns = [
     # Метод для выхода пользователя
     path(r'logout/', logout, name='logout'),
+
+    # Метод для выхода пользователя
+    path(r'login/', login, name='login'),
+
+    # Страница с формой для входа на сайт
+    path(r'login-page/', TemplateView.as_view(template_name='login_page.html'), name='login_page'),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,12 +1,25 @@
 """Файл с представлениями приложение users_app."""
 
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.shortcuts import redirect
+
+from lib.utils import multipart_to_dict
 
 
 def logout(request):
     """Функция для разлогинивания пользователя."""
     if request.user.is_authenticated:
         django_logout(request)
+
+    return redirect('posts_app:post_list')
+
+
+def login(request):
+    """Функция для входа пользователей пользователя."""
+    data = multipart_to_dict(request.POST)
+
+    user = authenticate(request, **data)
+    if user:
+        django_login(request, user)
 
     return redirect('posts_app:post_list')
