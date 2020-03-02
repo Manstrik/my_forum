d3285fba0ddc3e94c0eda69509e80c595b3e6fb0
nikolay <zhigalin@fas.gov.ru>
2020-03-02 17:14:08 +0300

    Добавлена настройка БД для dev-режима (общая)

--- my_forum/settings_dev.py
+++ my_forum/settings_dev.py
@@ -21,6 +21,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+
 try:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql',
