8e49f5538d346ced4b0b90b6ea6300c109aae853
Николай <nick.zhigalin@gmail.com>
2020-06-19 16:39:53 +0300

    Убраы лишние вертикальные пробелы

--- posts_app/urls.py
+++ posts_app/urls.py
@@ -10,13 +10,10 @@ app_name = 'posts_app'
 urlpatterns = [
     # Главная страница приложения со списком постов
     path(r'all/', PostList.as_view(), name='post_list'),
-
     # Страница с детальной информацией о посте
     path(r'<int:pk>/', PostDetail.as_view(), name='post_detail'),
-
     # Страница с формой для создания нвого поста
     path(r'new/', TemplateView.as_view(template_name='post_create.html'), name='new_post'),
-
     # API-функция для создания поста
     path(r'create/', create_post, name='create')
 ]
